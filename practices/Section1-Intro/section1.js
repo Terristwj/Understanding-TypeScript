0c70a3e65d4b5a0fbfb8708ccf91b02d577424a8
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-06 19:39:48 +0800

    Update

--- practices/Section1-Intro/section1.js
+++ practices/Section1-Intro/section1.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
--- practices/Section1-Intro/section1.ts
+++ practices/Section1-Intro/section1.ts
@@ -3,11 +3,9 @@ const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
 
 function add(num1: number, num2: number) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
-button.addEventListener("click", function() {
-    console.log(add(+input1.value, +input2.value));
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
 });
-
-export{};
