d68fa5851005e5ddbc5e2a10011b11327141a804
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-06 18:31:09 +0800

    format

--- practices/Section1-Intro/section1.js
+++ practices/Section1-Intro/section1.js
@@ -4,8 +4,8 @@ var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
