e50471622bc074b385320526e232a00aa70a11c8
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-09 10:20:40 +0800

    update

--- practices/Section2-Basics/practice.js
+++ practices/Section2-Basics/practice.js
@@ -1,20 +1,20 @@
 // 11) Types
-var type1 = 1.3; // All numbers, int & float
-var type2 = "a"; // Single quote '', Double quote "", Backtick ``
-var type3 = true;
-var type4;
-var type5; // Array
-var type6; // Tuple
+let type1 = 1.3; // All numbers, int & float
+let type2 = "a"; // Single quote '', Double quote "", Backtick ``
+let type3 = true;
+let type4;
+let type5; // Array
+let type6; // Tuple
 var type7;
 (function (type7) {
 })(type7 || (type7 = {}));
-var type8;
-var type9; // Union
-var type10; // Literal
-var type12 = function () { }; // Function
-var type13;
+let type8;
+let type9; // Union
+let type10; // Literal
+let type12 = () => { }; // Function
+let type13;
 type13(type12); // Function w/ callback
-var type14; // Unknown - More restrictive than 'any' (Must if-else check before using)
+let type14; // Unknown - More restrictive than 'any' (Must if-else check before using)
 // Never - only used when the function never ends
 function type15(errorMsg, code) {
     throw { message: errorMsg, errorCode: code };
@@ -23,40 +23,40 @@ function type15(errorMsg, code) {
 console.log(typeof type1);
 console.log(typeof type1 === "number");
 // 16) Types - Object
-var object1 = {
+let object1 = {
     name: "Adam",
     age: 14,
 };
 console.log(object1.name);
 // Explicit typing
-var object2 = {
+let object2 = {
     name: "Shamus",
     age: 15,
 };
 console.log(object2.name);
 // Literal typing
-var object3;
+let object3;
 object3 = {
     name: "James",
     age: 16,
 };
 console.log(object3.name);
 // 18) Types - Arrays
-var arr1;
+let arr1;
 arr1 = ["Sports"];
-var arr2; // Not recommended
+let arr2; // Not recommended
 arr2 = ["String", 1, true];
-var arr3 = [1, 2, 3];
+let arr3 = [1, 2, 3];
 // 19) Types - Tuples
-var tup1 = [1, 2, 3];
-var tup2;
+let tup1 = [1, 2, 3];
+let tup2;
 // 20) Types - Enums
 var RoleSet1;
 (function (RoleSet1) {
     RoleSet1[RoleSet1["ADMIN"] = 0] = "ADMIN";
     RoleSet1[RoleSet1["USER"] = 1] = "USER";
 })(RoleSet1 || (RoleSet1 = {}));
-var personRole = RoleSet1.ADMIN;
+let personRole = RoleSet1.ADMIN;
 console.log(personRole);
 var RoleSet2;
 (function (RoleSet2) {
@@ -71,12 +71,12 @@ var RoleSet3;
     RoleSet3["ADMIN"] = "administrator";
     RoleSet3[RoleSet3["USER"] = 15] = "USER";
 })(RoleSet3 || (RoleSet3 = {}));
-var personRole2 = RoleSet3.ADMIN;
+let personRole2 = RoleSet3.ADMIN;
 console.log(personRole2);
 personRole2 = RoleSet3.USER;
 console.log(personRole2);
 // 22) Union Types
-var unionVar = "a";
+let unionVar = "a";
 unionVar = 1;
 // 23) Literal Types
 function combine(input1, input2, conversionType // Literal Types
@@ -91,9 +91,9 @@ function combine(input1, input2, conversionType // Literal Types
 console.log(combine(1, 2, "as-number"));
 console.log(combine("Tom", "Sham", "as-number"));
 console.log(combine("Tom", "Sham", "as-string"));
-var sampleVar = "a";
+let sampleVar = "a";
 sampleVar = 1;
-var curMeal = "main course";
+const curMeal = "main course";
 // 26) Function Return Types & 'void'
 function undefinedFunction(n1, n2) {
     return;
@@ -103,47 +103,47 @@ function voidFunction() {
     console.log("Does nothing");
 }
 // 27) Function as Types
-var printVoid = voidFunction;
+let printVoid = voidFunction;
 printVoid();
-printVoid = function () {
+printVoid = () => {
     console.log("Does something");
 };
 printVoid();
 // Variable with fixed I/O function
-var addTwoNumbers;
-addTwoNumbers = function (n1, n2) {
+let addTwoNumbers;
+addTwoNumbers = (n1, n2) => {
     return n1 + n2;
 };
 console.log(addTwoNumbers(5, 5)); // OUT: 10
 // 28) Function Types & Callbacks
 // A callback is a function passed as an argument to another function
 function addNumbersInArray(arr1, addNum) {
-    var total = 0;
-    arr1.forEach(function (num) {
+    let total = 0;
+    arr1.forEach((num) => {
         total = addNum(total, num);
     });
     return total;
 }
-var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(addNumbersInArray(numberArray, addTwoNumbers)); // OUT: 45
 // Above is the same as below
 function addNumbersInArray2(arr1) {
-    var total = 0;
-    arr1.forEach(function (num) {
+    let total = 0;
+    arr1.forEach((num) => {
         total += num;
     });
     return total;
 }
 console.log(addNumbersInArray2(numberArray)); // OUT: 45
 // 29) The 'unknown' Type
-var itemUnkown;
-var aString;
+let itemUnkown;
+let aString;
 itemUnkown = "Can";
 if (typeof itemUnkown === "string") {
     aString = itemUnkown;
 }
 // Unkown vs Any
-var itemAny;
+let itemAny;
 itemAny = "Cup";
 aString = itemAny;
 // 30) The 'never' Type
